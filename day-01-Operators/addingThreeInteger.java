c3024e111d0d14590392574c492f4dc46fac3cbf
Shrinivas Madival <shrinivasshridharmadival.iet@srinivasuniversity.edu.in>
2026-06-23 22:05:30 +0530

    addingThreeInteger

---
+++ day-01-Operators/addingThreeInteger.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class addingThreeInteger {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        System.out.println(m+n+k);
+    }
+}
