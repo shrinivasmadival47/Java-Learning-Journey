059611e256541853c1b5d851e2cdc0e5338bde29
Shrinivas Madival <shrinivasshridharmadival.iet@srinivasuniversity.edu.in>
2026-06-25 21:57:06 +0530

    java code

---
+++ day-01-Operators/areaOfRectangle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class areaOfRectangle{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+        System.err.println(l*b);
+    }
+}
