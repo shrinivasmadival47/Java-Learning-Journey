287e8738e428ae81421d3e742f72ee4764c86be8
Shrinivas Madival <shrinivasshridharmadival.iet@srinivasuniversity.edu.in>
2026-06-30 22:58:07 +0530

    Second Largest Array Element

---
+++ day-01-Operators/SecondLargeArrayEle.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class SecondLargeArrayEle {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int secondLargest = arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest Number = " + secondLargest);
+    }
+
+    
+}
